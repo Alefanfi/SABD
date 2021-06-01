28801710a561af9de96c374bc2b1481e07317cf4
alefanfi <alefanfi@gmail.com>
2021-06-01 17:39:14 +0200

    Modificata sql_query2

--- src/main/java/sql_queries/Query2.java
+++ src/main/java/sql_queries/Query2.java
@@ -1,133 +1,104 @@
 package sql_queries;
 
 import comparator.Tuple3Comparator;
-
 import org.apache.commons.collections.IteratorUtils;
-import org.apache.log4j.LogManager;
-import org.apache.log4j.Logger;
+import org.apache.commons.math3.stat.regression.SimpleRegression;
 import org.apache.spark.api.java.JavaPairRDD;
-import org.apache.spark.ml.feature.VectorAssembler;
-import org.apache.spark.ml.linalg.Vectors;
-import org.apache.spark.ml.regression.LinearRegression;
-import org.apache.spark.ml.regression.LinearRegressionModel;
+import org.apache.spark.api.java.function.MapFunction;
 import org.apache.spark.sql.*;
+import org.apache.spark.sql.catalyst.encoders.RowEncoder;
+import org.apache.spark.sql.types.DataTypes;
+import org.apache.spark.sql.types.StructField;
+import org.apache.spark.sql.types.StructType;
 import scala.Tuple2;
 import scala.Tuple3;
-import scala.Tuple4;
 
-import java.text.ParseException;
 import java.text.SimpleDateFormat;
 import java.util.*;
-import java.util.stream.Collectors;
+
 
 public class Query2 {
 
-    private static final String outputPath = "hdfs://namenode:9000/spark/query2/";
     private static final String inputPath = "hdfs://namenode:9000/data/somministrazione-vaccini.parquet";
 
-    private static final Logger log = LogManager.getLogger(sql_queries.Query2.class.getName());
-
-    public static void main(String[] args) throws ParseException {
+    public static void main(String[] args) {
 
         SimpleDateFormat year_month_day_format = new SimpleDateFormat("dd-MM-yyyy");
 
         SparkSession spark = SparkSession
                 .builder()
-                .appName("SQL Query2")
+                .appName("Query2")
                 .master("spark://spark:7077")
                 .getOrCreate();
 
         Dataset<Row> dataset = spark.read().parquet(inputPath);
 
-        dataset.createOrReplaceTempView("data");
+        dataset.createOrReplaceTempView("dati");
 
-        Dataset<Row> sqlDF = spark.sql("SELECT data_somministrazione, nome_area, fascia_anagrafica, CAST(sesso_femminile AS INT) AS sesso_femminile " +
-                "FROM data WHERE DATE(data_somministrazione) > DATE('2021-1-31')");
+        //take all value after the 31-01-2021
+        Dataset<Row> sqlDF = spark.sql("SELECT data_somministrazione AS date, nome_area AS area, fascia_anagrafica AS age, sesso_femminile " +
+                "FROM dati WHERE DATE(data_somministrazione) > DATE('2021-1-31')");
 
-        sqlDF = sqlDF
-                .groupBy("data_somministrazione", "nome_area", "fascia_anagrafica") // Grouping by date, region, age
-                .sum("sesso_femminile") // Sum up all vaccinations in a single day
-                .withColumn("mese_anno", functions.concat(
+        sqlDF = sqlDF.withColumn("sesso_femminile", sqlDF.col("sesso_femminile").cast("long"))
+                .groupBy("date", "area", "age")
+                .sum("sesso_femminile");
 
-                    // Creating new column with only year and month of date
-                    functions.month(sqlDF.col("data_somministrazione")),
-                    functions.lit("-"),
-                    functions.year(sqlDF.col("data_somministrazione")))
-                )
-                .withColumn("key", functions.concat(
+        //create a new column with month and year value
+        sqlDF = sqlDF.withColumn("month_year", functions.concat(
+                functions.month(sqlDF.col("date")),
+                functions.lit("-"),
+                functions.year(sqlDF.col("date"))));
 
-                        // Creating key with date-age-region
-                        sqlDF.col("mese_anno"), functions.lit(" - "),
-                        sqlDF.col("fascia_anagrafica"), functions.lit(" - "), sqlDF.col("nome_area"))
-                )
-                .withColumn("data_somministrazione", sqlDF.col("data_somministrazione").cast("timestamp").cast("long")) // Casting date to long
-                .withColumn("vaccini", sqlDF.col("sum(sesso_femminile)").cast("long")) // Casting vacc number to long
-                .drop("sum(sesso_femminile)", "nome_area", "fascia_anagrafica")
-                .sort("key", "data_somministrazione");
+        StructType schema = DataTypes.createStructType(new StructField[] {
+                DataTypes.createStructField("date",  DataTypes.StringType, true),
+                DataTypes.createStructField("region",  DataTypes.StringType, true),
+                DataTypes.createStructField("age",  DataTypes.StringType, true),
+                DataTypes.createStructField("predicted", DataTypes.IntegerType, true)
+        });
 
-        sqlDF.show();
 
-        List<String> keyString = sqlDF
-                .select(sqlDF.col("key"))
-                .distinct()
-                .collectAsList()
-                .stream()
-                .map(r -> r.getString(0)).sorted(String.CASE_INSENSITIVE_ORDER).collect(Collectors.toList());
+        Dataset<Row> prediction_dt = sqlDF
+                .withColumn("date", sqlDF.col("date").cast("timestamp").cast("long"))
+                .withColumn("vaccini", sqlDF.col("sum(sesso_femminile)").cast("long"))
+                .withColumn("value", functions.struct("date", "vaccini"))
+                .drop(sqlDF.col("sum(sesso_femminile)"))
+                .sort("month_year", "area", "age")
+                .groupBy("month_year", "area", "age")
+                .agg(functions.collect_list("value"))//(mont_year, area, age) [](data, vaccini)
+                .map((MapFunction<Row, Row>) row->{
 
-        List<Tuple4<String, String, String, Long>> prediction = new ArrayList<>();
+                    List<Row> data = row.getList(3);
 
-        for (String s : keyString) {
+                    SimpleRegression simpleRegression = new SimpleRegression();
+                    data.forEach(x -> simpleRegression.addData(x.getLong(0)*1000, x.getLong(1)));
 
-            Dataset<Row> dt = sqlDF.filter(sqlDF.col("key").equalTo(s));
+                    // Get next month for prediction
+                    Date date = year_month_day_format.parse("01-0" + row.getString(0));
+                    Calendar cal = Calendar.getInstance();
+                    cal.setTime(date);
+                    cal.add(Calendar.MONTH, 1);
+                    Date nextMonth = cal.getTime();
+                    String nextMonthString = year_month_day_format.format(nextMonth);
 
-            VectorAssembler assembler = new VectorAssembler()
-                    .setInputCols(new String[]{"data_somministrazione"})
-                    .setOutputCol("features");
+                    double predict = simpleRegression.predict((double) nextMonth.getTime());
 
-            LinearRegression lr = new LinearRegression()
-                    .setMaxIter(5)
-                    .setRegParam(0.3)
-                    .setElasticNetParam(0.8)
-                    .setFeaturesCol("features")
-                    .setLabelCol("vaccini");
+                    return RowFactory.create(nextMonthString, row.getString(1), row.getString(2), (int) Math.round(predict)); //date, region, age, predict
 
-            // Fit the model
-            LinearRegressionModel lrModel = lr.fit(assembler.transform(dt));
+                }, RowEncoder.apply(schema));
 
-            //Get current date
-            String[] tmp = s.split(" ");
-            Date date = year_month_day_format.parse("01-0" + tmp[0]);
+        prediction_dt.show();
 
-            // Get next month for prediction
-            Calendar cal = Calendar.getInstance();
-            cal.setTime(date);
-            cal.add(Calendar.MONTH, 1);
-            Date newdate = cal.getTime();
-            String newdateString = year_month_day_format.format(newdate);
-
-            // Predict women vaccinations the first day of the next month
-            double predict = lrModel.predict(Vectors.dense(newdate.getTime()));
-
-            prediction.add(new Tuple4<>(newdateString, tmp[2], tmp[4], (long) predict));
-
-        }
-
-        Encoder<Tuple4<String, String, String, Long>> encoder = Encoders.tuple(Encoders.STRING(),
-                Encoders.STRING(), Encoders.STRING(), Encoders.LONG());
-
-        JavaPairRDD<Tuple3<String, String, Integer>, String> predictRDD = spark
-                .createDataset(prediction, encoder)
-                .toDF("Date", "Age", "Area", "Vaccini")
+        JavaPairRDD<Tuple3<String, String, Integer>, String> predict_rdd = prediction_dt
                 .toJavaRDD()
                 .mapToPair(
                         row ->
-                                new Tuple2<>(new Tuple2<>(row.getString(1), row.getString(2)), new Tuple2<>( row.getLong(0), row.getString(3))) // ((year_month, age),(num_women_vacc, region))
+                                new Tuple2<>(new Tuple2<>(row.getString(0), row.getString(2)), new Tuple2<>(row.getString(1), row.getInt(3))) // ((year_month, age),(region, num_women_vacc))
                 )
-                .groupByKey() // ((year_month, age), [](num_women_vacc, region))
+                .groupByKey()//((year_month, age), [](region, num_women_vacc))
                 .flatMapToPair(
                         x -> {
 
-                            List<Tuple2<Long,String>> scores = IteratorUtils.toList(x._2.iterator());
+                            List<Tuple2<Integer, String>> scores = IteratorUtils.toList(x._2.iterator());
                             scores.sort(Comparator.comparing(n -> n._1)); // Sorting by num_women_vacc
                             Collections.reverse(scores);
 
@@ -138,22 +109,20 @@ public class Query2 {
 
                             for(int i=0; i<5; i++){
 
-                                Tuple2<Long, String> tupla = scores.get(i);
+                                Tuple2<Integer, String> tupla = scores.get(i);
 
-                                newlist.add(new Tuple2<>(new Tuple3<>(date, age, tupla._1.intValue()), tupla._2)); // ((year_month, age, num_women_vacc), region)
+                                newlist.add(new Tuple2<>(new Tuple3<>(date, age, tupla._1), tupla._2)); // ((year_month, age, num_women_vacc), region)
                             }
                             return newlist.iterator();
                         })
                 .sortByKey(new Tuple3Comparator<>(Comparator.<String>naturalOrder(), Comparator.<String>naturalOrder(), Comparator.<Integer>reverseOrder()), true); // Sorts by date, age and num_women_vacc
 
-        Encoder<Tuple2<Tuple3<String, String, Integer>, String>> encoder2 = Encoders.tuple(Encoders.tuple
-                (Encoders.STRING(), Encoders.STRING(), Encoders.INT()), Encoders.STRING());
-
-        Dataset<Row> output_dt = spark.createDataset(JavaPairRDD.toRDD(predictRDD), encoder2)
+        Dataset<Row> output_dt = spark.createDataset(JavaPairRDD.toRDD(predict_rdd), Encoders.tuple(Encoders.tuple
+                (Encoders.STRING(), Encoders.STRING(), Encoders.INT()), Encoders.STRING()))
                 .toDF("key", "value")
                 .selectExpr("key._1 as date", "key._2 as age", "value as region", "key._3 as vacc_women");
 
-        output_dt.write().mode(SaveMode.Overwrite).option("header","true").csv(outputPath);
+        output_dt.show();
 
         spark.close();
     }
